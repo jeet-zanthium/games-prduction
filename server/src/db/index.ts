fd60325b631ddd261a44718ac138f22db3ac2336
Jeet Gangwar <jeetzanthium@gmail.com>
2024-12-07 10:28:21 +0530

    did some optimizations

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,5 +1,32 @@
 import { PrismaClient } from '@prisma/client';
+import chalk from 'chalk';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+});
+
+prisma.$on('query', (e) => {
+  console.log(chalk.cyan('Query: ') + chalk.yellow(e.query));
+  console.log(chalk.cyan('Params: ') + chalk.green(e.params));
+  console.log(chalk.cyan('Duration: ') + chalk.magenta(e.duration + 'ms'));
+  console.log(chalk.red('----------------------------------'));
+});
 
 export { prisma };
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,8 @@ import app from '@/app/app';
 import { env } from '@/config/env';
 import { initSocketServer } from '@/sockets/socket.server';
 
+import { prisma } from './db';
+
 // Create HTTP server and attach Express app
 const PORT = env.PORT || 3000;
 const httpServer = createServer(app);
@@ -22,4 +24,21 @@ app.io = io;
 // Start server
 httpServer.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+process.on('SIGINT', async () => {
+  console.log('Shutting down server...');
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+  console.log('Server is terminating...');
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+httpServer.on('close', async () => {
+  console.log('Server is shutting down...');
+  await prisma.$disconnect();
 });
